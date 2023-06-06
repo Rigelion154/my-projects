c6f1d99cd4ec648006a478b31bbb26c85555a8c1
Ilya Kyznetsov <cupofbeer123@gmail.com>
2023-06-06 21:51:28 +0300

    feat: add typescript

--- migration-newip-to-ts/src/components/controller/loader.js
+++ migration-newip-to-ts/src/components/controller/loader.js
@@ -44,3 +44,6 @@ class Loader {
 }
 
 export default Loader;
+
+
+
--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -4,13 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i, use: 'ts-loader'
             },
         ],
     },
