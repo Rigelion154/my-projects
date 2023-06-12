e5978ee11f312a7e9ca9beda872c17edfe08992d
Ilya Kyznetsov <cupofbeer123@gmail.com>
2023-06-12 17:18:12 +0300

    fix: fix proxy link for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '703884a6aa664aaaa520353cd4a2f4fd', // получите свой ключ https://newsapi.org/
         });
     }
